23536b9400ecc7d6733e1bb50cc848f3c035faef
we <444202104@qq.com>
2018-12-13 16:49:12 +0800

    背景 结构

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -1,6 +1,37 @@
 <template>
-  <div class="dashboard-editor-container">
-    <h1>首页</h1>
+  <div class="dashboard-editor-container flex-between">
+    <!--左-->
+    <div class="left">
+      <!--左上-->
+      <div class="left-top shadowBack">左上</div>
+      <!--左中-->
+      <div class="left-middle shadowBack">左中</div>
+      <!--左下-->
+      <div class="left-bottom shadowBack">左下</div>
+    </div>
+    <!--中-->
+    <div class="middle">
+      <!--中上-->
+      <div class="middle-top shadowBack">中上</div>
+      <!--中下-->
+      <div class="middle-bottom flex-between">
+        <!--中下左-->
+        <div class="middle-bottom-left shadowBack">中下左</div>
+        <!--中下右-->
+        <div class="middle-bottom-right shadowBack">中下右</div>
+      </div>
+    </div>
+    <!--右-->
+    <div class="right">
+      <!--右1-->
+      <div class="right-one shadowBack">右1</div>
+      <!--右2-->
+      <div class="right-two shadowBack">右2</div>
+      <!--右3-->
+      <div class="right-three shadowBack">右3</div>
+      <!--右4-->
+      <div class="right-four shadowBack">右4</div>
+    </div>
   </div>
 </template>
 
@@ -20,7 +51,49 @@ export default {
 
 <style rel="stylesheet/scss" lang="scss" scoped>
 .dashboard-editor-container {
-  padding: 32px;
-  background-color: rgb(240, 242, 245);
+  padding: 5px;
+  background-color: #0d1733;
+  .left {
+    width: 25%;
+    .left-top {
+      width: 100%;
+    }
+    .left-middle {
+      width: 100%;
+    }
+    .left-bottom {
+      width: 100%;
+    }
+  }
+  .middle {
+    width: 50%;
+    .middle-top {
+      width: 100%;
+    }
+    .middle-bottom {
+      width: 100%;
+      .middle-bottom-left {
+        width: 50%;
+      }
+      .middle-bottom-right {
+        width: 50%;
+      }
+    }
+  }
+  .right {
+    width: 25%;
+    .right-one {
+      width: 100%;  
+    }
+    .right-two {
+      width: 100%;  
+    }
+    .right-three {
+      width: 100%;  
+    }
+    .right-four {
+      width: 100%;  
+    }
+  }
 }
 </style>
