d88243134e58cbb2cc8147a4d18e459684e066cb
we <444202104@qq.com>
2018-12-29 14:10:40 +0800

    资源分布

--- src/views/weatherEnvironment/index.vue
+++ src/views/weatherEnvironment/index.vue
@@ -1,13 +1,28 @@
 <template>
-  <h1>天气环境</h1>
+  <div class="weatherEnvironment">
+    <div @click="fuck">资源分布</div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    fuck() {
+      window.location.href = 'http://61.174.54.82:18007/login'
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .weatherEnvironment {
+    position: relative;
+    z-index: 999;
+    div {
+      color: #ffffff;
+      font-size: 30px;
+      margin: 10px;
+    }
+  }
 </style>
+
