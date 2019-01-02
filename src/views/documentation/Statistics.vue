602c5fc97cd2e5162dacfb12e2d9b381934983f2
we <444202104@qq.com>
2019-01-02 17:40:03 +0800

    事件统计挂俩图

--- src/views/documentation/Statistics.vue
+++ src/views/documentation/Statistics.vue
@@ -5,7 +5,7 @@
       <span>选择时间： </span>
       <el-date-picker
         v-model="value4"
-        type="datetimerange"
+        type="daterange"
         range-separator="至"
         start-placeholder="开始日期"
         end-placeholder="结束日期"/>
@@ -83,10 +83,49 @@
         layout="total, sizes, prev, pager, next, jumper"
         :total="400"/>
     </div>
+    <!--关键字搜索-->
+    <div class="main1">
+      <div class="top-box">
+        <span>选择时间： </span>
+        <el-date-picker
+          v-model="value4"
+          type="daterange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"/>
+        <!--查询-->
+        <el-button size="mini" type="primary">查询</el-button>
+      </div>
+      <!--两个图-->
+      <div class="two-box">
+        <!--事件统计-->
+        <div class="left-box">
+          <!--标题-->
+          <div class="title">事件处理情况统计</div>
+          <div class="content">
+            <div class="chart-wrapper">
+              <Holiday-sjcl height="100%" width="100%"/>
+            </div>
+          </div>
+        </div>
+        <!--事件处理率-->
+        <div class="right-box">
+          <!--标题-->
+          <div class="title">事件处理率</div>
+          <div class="content">
+            <div class="chart-wrapper">
+              <yi-zhou :fatherData="yktype" height="100%" width="100%"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import HolidaySjcl from '@/components/Charts/holiday-sjcl'
+import YiZhou from '@/views/dashboard/admin/components/YiZhou'
 export default {
   data() {
     return {
@@ -115,7 +154,7 @@ export default {
         }
       ],
       currentPage4: 4,
-      value4: [new Date(2000, 10, 10, 10, 10), new Date(2000, 10, 11, 10, 10)],
+      value4: [new Date(), new Date()],
       options: [
         {
           value: '选项1',
@@ -136,6 +175,9 @@ export default {
       ],
       value: '选项1'
     }
+  },
+  components: {
+    HolidaySjcl, YiZhou
   },
   methods: {
     handleEdit(index, row) {
@@ -171,6 +213,10 @@ export default {
     position: relative;
     z-index: 999;
     padding: 1%;
+    .el-date-editor .el-range-separator {
+      padding: 0!important;
+      color: #000!important;
+    }
     .main {
       display: flex;
       justify-content: flex-start;
@@ -188,6 +234,74 @@ export default {
       } 
       .el-button--mini {
         margin-left: 2%;
+      }
+    }
+    .main1 {
+      display: flex;
+      flex-direction: column;
+      background: rgba(255, 255, 255, 0.05);
+      padding: 0 1%;
+      .top-box {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        margin: 1% 0;
+        span {
+          color: #ffffff;
+          margin-right: 2%;
+          font-size: 20px;
+        }
+        .fuckyou {
+          margin-left: 3%;
+        } 
+        .el-button--mini {
+          margin-left: 2%;
+        }
+      }
+      .two-box {
+        display: flex;
+        justify-content: space-between;
+        .left-box {
+          display: flex;
+          flex-direction: column;
+          flex: 1;
+          margin-right: 1%;
+          background: rgba(255, 255, 255, 0.05);
+          height: 320px;
+          .title {
+            font-size: 22px;
+            color: #bbd5ff;
+            background: #45404d;
+            padding: 1%;
+          }
+          .content {
+            flex: 1;
+            .chart-wrapper {
+              width: 100%;
+              height: 100%;
+            }
+          }
+        }
+        .right-box {
+          display: flex;
+          flex-direction: column;
+          flex: 1;
+          height: 320px;
+          background: rgba(255, 255, 255, 0.05);
+          .title {
+            font-size: 22px;
+            color: #bbd5ff;
+            background: #45404d;
+            padding: 1%;
+          }
+          .content {
+            flex: 1;
+            .chart-wrapper {
+              width: 100%;
+              height: 100%;
+            }
+          }
+        }
       }
     }
     // .el-table,
