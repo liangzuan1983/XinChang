cfae9ce5a858ee45fe2b2bb551f01b6186122826
we <444202104@qq.com>
2019-01-09 16:17:01 +0800

    警告

--- src/views/documentation/index.vue
+++ src/views/documentation/index.vue
@@ -6,24 +6,29 @@
         :center="center"
         :zoom="zoom"
         class="amap-demo">
-        <el-amap-marker v-for="(marker, index) in markers" :key="index" :position="marker.position" :vid="index">
+        <!-- <el-amap-marker v-for="(marker, index) in markers" :key="index" :position="marker.position" :vid="index">
           <div :style="slotStyle">
-            <b>Hello {{ count }} times</b>
-            <button @click="onClick">Add</button>
+            <img src="@/assets/jk.png" alt="" srcset="" @click="onClick">
+          </div>
+          <div :style="slotStyle2">
+            <img src="@/assets/lou.png" alt="" srcset="" @click="onClick">
+          </div>
+        </el-amap-marker> -->
+        <el-amap-marker :position="markers" :vid="index">
+          <div :style="slotStyle">
+            <img src="@/assets/jk.png" alt="" srcset="" @click="onClickT">
+          </div>
+        </el-amap-marker>
+        <el-amap-marker :position="markers2" :vid="index">
+          <div :style="slotStyle2">
+            <img src="@/assets/lou.png" alt="" srcset="" @click="onClickB">
           </div>
         </el-amap-marker>
       </el-amap>
     </div>
     <!--浮窗-->
-    <!-- <div class="float">
-      <div class="top">
-        <h1 class="title">事件统计</h1>
-        <p>事件总数: 10</p>
-        <p>待处理事件: 3</p>
-        <p>处理中事件: 4</p>
-        <p>已完结事件: 4</p>
-      </div>
-      <div class="bottom">
+    <div class="float">
+      <div class="top" v-if="top">
         <h1 class="title">预警事件信息</h1>
         <p>编号: 0001</p>
         <p>时间: 2018.8.18 12:05:03</p>
@@ -31,7 +36,15 @@
         <p>详情: 客流超载预警</p>
         <p>预案: 应急预案3</p>
       </div>
-    </div> -->
+      <div class="bottom" v-if="bottom">
+        <h1 class="title">预警事件信息</h1>
+        <p>编号: 0003</p>
+        <p>时间: 2019.1.2 16:15:43</p>
+        <p>地点: 十九峰</p>
+        <p>详情: 人群稍微拥挤</p>
+        <p>预案: 应急预案1</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -43,34 +56,57 @@ export default {
     return {
       zoom: 12,
       center,
-      markers: [],
+      // markers: [],
+      markers: [120.897454,29.506942],
+      markers2: [120.954782,29.462718],
       count: 0,
       slotStyle: {
         padding: '2px 8px',
-        background: '#eee',
+        background: 'red',
         color: '#333',
-        border: '1px solid #aaa'
-      }
+        border: '1px solid #aaa',
+        borderRadius: '10px'
+      },
+      slotStyle2: {
+        padding: '2px 8px',
+        background: 'green',
+        color: '#333',
+        border: '1px solid #aaa',
+        borderRadius: '10px'
+      },
+      top: false,
+      bottom: false
     }
   },
   created() {
-    let self = this;
-    let markers = [];
-    let index = 0;
+    // let self = this;
+    // let markers = [];
+    // let index = 0;
 
-    let basePosition = [120.897454,29.506942];
-    let num = 3;
+    // let basePosition = [120.897454,29.506942];
+    // let num = 3;
 
-    for (let i = 0 ; i < num ; i++) {
-      markers.push({
-        position: [basePosition[0], basePosition[1] + i * 0.03]
-      });
-    }
-    this.markers = markers;
+    // for (let i = 0 ; i < num ; i++) {
+    //   markers.push({
+    //     position: [basePosition[0], basePosition[1] + i * 0.03]
+    //   });
+    // }
+    // this.markers = markers;
   },
   methods: {
-    onClick() {
-      this.count += 1;
+    onClickT() {
+      if (this.top === true) {
+        this.top = false
+      } else if (this.top === false) {
+        this.top = true
+      }
+    },
+    onClickB() {
+      if (this.bottom === true) {
+        this.bottom = false
+      } else if (this.bottom === false) {
+        this.bottom = true
+      }
     }
   }
 }
