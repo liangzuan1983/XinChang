4a024dbaf9893ae3ab65c88c9318484d9a69fa70
we <444202104@qq.com>
2019-02-12 10:15:30 +0800

    '应急预案-事件统计浮窗'

--- src/views/documentation/index.vue
+++ src/views/documentation/index.vue
@@ -6,14 +6,6 @@
         :center="center"
         :zoom="zoom"
         class="amap-demo">
-        <!-- <el-amap-marker v-for="(marker, index) in markers" :key="index" :position="marker.position" :vid="index">
-          <div :style="slotStyle">
-            <img src="@/assets/jk.png" alt="" srcset="" @click="onClick">
-          </div>
-          <div :style="slotStyle2">
-            <img src="@/assets/lou.png" alt="" srcset="" @click="onClick">
-          </div>
-        </el-amap-marker> -->
         <el-amap-marker :position="markers" :vid="index">
           <div :style="slotStyle">
             <img src="@/assets/jk.png" alt="" srcset="" @click="onClickT">
@@ -29,20 +21,11 @@
     <!--浮窗-->
     <div class="float">
       <div class="top" v-if="top">
-        <h1 class="title">预警事件信息</h1>
-        <p>编号: 0001</p>
-        <p>时间: 2018.8.18 12:05:03</p>
-        <p>地点: 大佛寺</p>
-        <p>详情: 客流超载预警</p>
-        <p>预案: 应急预案3</p>
-      </div>
-      <div class="bottom" v-if="bottom">
-        <h1 class="title">预警事件信息</h1>
-        <p>编号: 0003</p>
-        <p>时间: 2019.1.2 16:15:43</p>
-        <p>地点: 十九峰</p>
-        <p>详情: 人群稍微拥挤</p>
-        <p>预案: 应急预案1</p>
+        <h1 class="title">事件统计</h1>
+        <p>事件总数: 10</p>
+        <p>待处理事件: 3</p>
+        <p>处理中事件: 4</p>
+        <p>已完结事件: 4</p>
       </div>
     </div>
   </div>
@@ -74,24 +57,8 @@ export default {
         border: '1px solid #aaa',
         borderRadius: '10px'
       },
-      top: false,
-      bottom: false
+      top: true
     }
-  },
-  created() {
-    // let self = this;
-    // let markers = [];
-    // let index = 0;
-
-    // let basePosition = [120.897454,29.506942];
-    // let num = 3;
-
-    // for (let i = 0 ; i < num ; i++) {
-    //   markers.push({
-    //     position: [basePosition[0], basePosition[1] + i * 0.03]
-    //   });
-    // }
-    // this.markers = markers;
   },
   methods: {
     onClickT() {
@@ -144,23 +111,6 @@ export default {
         width: 100%;
         height: 30%;
         margin-bottom: 30%;
-        background: rgba(0, 0, 0, 0.7);
-        padding: 5%;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        h1 {
-          color: #ffffff;
-          font-size: 18px;
-        }
-        p {
-          color: #ffffff;
-          font-size: 16px;
-        }
-      }
-      .bottom {
-        width: 100%;
-        height: 30%;
         background: rgba(0, 0, 0, 0.7);
         padding: 5%;
         display: flex;
